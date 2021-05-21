ec39369600e279a22bef69fdcf9209dce9b3c68b
Wenyin Wei <wenyin.wei.ww@gmail.com>
2021-05-21 16:53:24 +0800

    fix setup description string

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="0.0.1",
     author="Wenyin Wei",
     author_email="wenyin.wei@ipp.ac.cn",
-    description="Silkpy is planned to be a math package for curves and surfaces which are classical objects of differential geometry. Silk, in Chinese, is a kind of soft clothe material with intervined perpendicular lines of longitude and latitude. The lines are named '丝 si' while the clothe is named '绸 chou', which are metaphors for the 'curve' and 'surface' in differential geometry.
+    description="Silkpy is planned to be a math package for curves and surfaces which are classical objects of differential geometry. Silk, in Chinese, is a kind of soft clothe material with intervined perpendicular lines of longitude and latitude. The lines are named '丝 si' while the clothe is named '绸 chou', which are metaphors for the 'curve' and 'surface' in differential geometry.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/WenyinWei/silkpy",
