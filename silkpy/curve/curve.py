794f745c29b635e3821f790d86190e8c060a39ab
Wenyin Wei <wenyin.wei.ww@gmail.com>
2021-05-30 16:51:25 +0800

    cached_property some methods in Curve

--- silkpy/curve/curve.py
+++ silkpy/curve/curve.py
@@ -1,6 +1,7 @@
 from sympy import Array as _Array
 from sympy import diff as _diff
 from sympy import sqrt as _sqrt
+from functools import cached_property
 
 
 from ..geometry_map import GeometryMap
@@ -25,19 +26,21 @@ class ParametricCurve(GeometryMap):
     def __or__(self, other):
         return self.chain(other)
 
-
-#     def r_t(self):
-#         return self._r.diff(self.sym(0))
+    @cached_property
+    def r_t(self):
+        return self._r.diff(self.sym(0))
     
     # def __str__(self):
     #     return f"A curve = {self._r} in {self._coord} coordinate system, with {self.sym(0)} in {self.sym(0)_limit}."
     
+    @cached_property
     def curvature(self): # k(t)
         from silkpy.sympy_utility import norm, cross 
         drdt = self.expr().diff(self.sym(0))
         d2rdt2 = drdt.diff(self.sym(0))
         return norm(cross(drdt, d2rdt2)) / norm(drdt)**3
-        
+    
+    @cached_property
     def torsion(self): # \tau(t)
         from silkpy.sympy_utility import norm, cross, triple_prod
         drdt = self.expr().diff(self.sym(0))
@@ -48,6 +51,7 @@ class ParametricCurve(GeometryMap):
         denominator = norm( cross( drdt, d2rdt2 ) )**3
         return numerator / denominator
 
+    @cached_property
     def T_N_B(self): # \vec{T}(t), \vec{N}(t), \vec{B}(t)
         from silkpy.sympy_utility import norm, cross
         drdt = self.expr().diff(self.sym(0))
