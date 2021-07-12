6ee9a712df159896b4cb9088762c9d2d969ce28c
Wenyin Wei <wenyin.wei.ww@gmail.com>
2021-07-12 21:35:49 +0800

    last time forgotten change

--- silkpy/numeric/geometry_map/geometry_map.py
+++ silkpy/numeric/geometry_map/geometry_map.py
@@ -1,13 +1,26 @@
 from functools import cached_property
+import numpy as _np
 
 class NGeometryMap:
     """Base class for geometry map.
     """
-    def __init__(self, syms, exprs):
+    def __init__(self, syms_arr, exprs_arr, syms=None, exprs=None):
+        for sym_arr in syms_arr:
+            assert( isinstance(sym_arr, _np.array) )
+            assert( sym_arr.ndim == 1)
+        for expr_arr in exprs_arr:
+            assert( isinstance(expr_arr, _np.array) )
+            assert( expr_arr.shape == tuple(sym_arr.size for sym_arr in syms_arr) )
+        self._syms_arr = list(syms_arr)
+        self._exprs_arr = list(exprs_arr)
+        
         from sympy import Array, oo
-        if not isinstance(exprs, list) and not isinstance(exprs, Array):
-            raise ValueError("The ctor arg of GeometryMap -- exprs -- should be a list of sympy expressions.")
-        self._exprs = Array(exprs)
+        if exprs is not None:
+            if not isinstance(exprs, list) and not isinstance(exprs, Array):
+                raise ValueError("The ctor arg of GeometryMap -- exprs -- should be a list of sympy expressions.")
+            self._exprs = Array(exprs)
+        else:
+            self._exprs = Array( [None]*len(exprs_arr) )
 
         # if not isinstance(syms, list):
         #     raise ValueError("The ctor arg of GeometryMap -- syms -- should be a list of sympy variables, or a list of (sympy.Symbol, inf, sup)")
@@ -27,8 +40,8 @@ class NGeometryMap:
             return self._exprs
         else:
             return self._exprs[i]
-    def subs(self, *arg):
-        return GeometryMap(self, self._exprs.subs(*arg), self._syms)
+    # def subs(self, *arg):
+    #     return GeometryMap(self, self._exprs.subs(*arg), self._syms)
 
     @property
     def syms(self):
@@ -55,7 +68,8 @@ class NGeometryMap:
                 Array(self.syms()))
 
     def lambdified(self, *arg, **kwarg):
-        from sympy import lambdify
-        return lambdify(
-            self.sym(), 
-            self.expr().tolist(), *arg, **kwarg)
+        return NotImplementedError()
+        # from sympy import lambdify
+        # return lambdify(
+        #     self.sym(), 
+        #     self.expr().tolist(), *arg, **kwarg)
