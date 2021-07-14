e655b3b2eee8f360d30a6d82804c55eff63ed071
Wenyin Wei <wenyin.wei.ww@gmail>
2021-07-14 08:54:26 +0800

    sym() -> syms

--- silkpy/symbolic/geometry_map/coord_transform/coord_transform.py
+++ silkpy/symbolic/geometry_map/coord_transform/coord_transform.py
@@ -6,19 +6,19 @@ class CoordTransform(_GeometryMap):
         from silkpy.symbolic.surface.surface import ParametricSurface
 
         # assert( self.expr().rank() == 1)
-        assert( len(other.sym()) == int(self.expr().shape.args[0]) )
+        assert( len(other.syms) == int(self.exprs.shape.args[0]) )
 
         if isinstance(other, ParametricCurve):
             return ParametricCurve(self._syms, other._exprs.subs({
-                other.sym(i): self.expr(i) for i in range(len(self.sym()))})
+                other.sym(i): self.expr(i) for i in range(len(self.syms))})
                 )
         elif isinstance(other, ParametricSurface):
             return ParametricSurface(self._syms, other._exprs.subs({
-                other.sym(i): self.expr(i) for i in range(len(self.sym()))})
+                other.sym(i): self.expr(i) for i in range(len(self.syms))})
                 )
         elif isinstance(other, CoordTransform):
             return CoordTransform(self._syms, other._exprs.subs({
-                other.sym(i): self.expr(i) for i in range(len(self.sym()))})
+                other.sym(i): self.expr(i) for i in range(len(self.syms))})
                 )
         else:
             raise TypeError("The chain succession must be a GeometryMap.")
